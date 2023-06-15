1f841b637dd3019623bbaa3431cffd3d5cd8a80d
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-06-15 11:58:59 -0500

    Finished Account Screen

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,13 +1,30 @@
-import { View, SafeAreaView, StyleSheet } from "react-native";
+import { View, SafeAreaView, StyleSheet, Image, Text, ScrollView } from "react-native";
+
 import Header from "../components/Header";
+import InputField from "../components/InputField";
 
 export default () => {
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{margin: 20}}>
-                <Header text={"Account"}/>
-                
-            </View>
+            <ScrollView>
+                <View style={{margin: 20}}>
+                    <Header text={"Account"}/>
+                    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                        <View style={styles.placeholder}>
+                            <Image 
+                                source={require('../assets/account-pic-placeholder.png')}
+                                style={{width: 100, height: 100}}
+                            />
+                        </View>
+                        <Text style={styles.name}>Jane Doe</Text>
+                        <InputField title='Username' placeholder='janedoe123'/>
+                        <InputField title='First Name' placeholder='Jane'/>
+                        <InputField title='Last Name' placeholder='Doe'/>
+                        <InputField title='E-mail Address' placeholder='janedoe123@gmail.com'/>
+                        <InputField title='Password' placeholder='*************'/>
+                    </View>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
@@ -18,4 +35,18 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff',
         flexDirection: 'column',
     },
+    placeholder: {
+        width: 200,
+        height: 200,
+        borderRadius: 200/2,
+        backgroundColor: '#D9D9D9',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    name: {
+        fontWeight: 600,
+        fontSize: 24,
+        margin: 10
+    }
 });
