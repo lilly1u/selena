f83c738eaef69f48c2f4fda27274e01128cd4039
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-07-07 13:15:01 -0500

    Cleared homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 import Card from '../components/Card';
 import Button from '../components/Button';
 
-const HomeScreen = ({navigation}) => { 
+const HomeScreen = () => { 
     return (
     <SafeAreaView style={styles.container}>
         <ScrollView>
@@ -14,17 +14,6 @@ const HomeScreen = ({navigation}) => {
                     <Image source={require('../assets/welcome-illustration.png')} style={{resizeMode:'cover', position:'absolute', alignSelf:'flex-end'}}/>
                     <Text style={styles.title}>Welcome to <Text style={{color: '#8E8E8E'}}>Social Emotional Learning Enhancement Application</Text></Text>
                 </View>
-                <Text style={styles.subheading}>Recent</Text>
-                <View style={{alignItems:'flex-start', flexDirection:'row'}}>
-                    <Card title='SELENA Primary Arabic' instructor='Swati Menon' image={{uri: 'https://myselena.org/wp-content/uploads/2023/04/qtq80-GtXhBl.jpeg'}}/>
-                    <Card title='SELENA Primary One-to-Ones Spanish' instructor='Absalaam Thomas' image={{uri: 'https://myselena.org/wp-content/uploads/2023/04/qtq80-a8SCCV.jpeg'}}/>
-                </View>
-                <Text style={styles.subheading}>Reccomended</Text>
-                <View style={{alignItems:'flex-start', flexDirection:'row'}}>
-                    <Card title='SELENA Middle School One-to-Ones English' instructor='Karlee Kategianes' image={{uri: 'https://myselena.org/wp-content/uploads/2023/04/qtq80-iGG6NX.jpeg'}}/>
-                    <Card title='SELENA High School Audio Scripts English' instructor='Absalaam Thomas' image={{uri: 'https://myselena.org/wp-content/uploads/2023/04/qtq80-LKOPTA.jpeg'}}/>
-                </View>
-                <Button title='Contact Us!' onPress={() => navigation.navigate('Contact')} buttonStyle={{width: 350, height: 50}}/>
             </View>
         </ScrollView>
     </SafeAreaView>
