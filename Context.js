7150db3b2f3e2cc4b2f1d08d33fd96f852e5703d
Lilly Lu <98109844+lilly1u@users.noreply.github.com>
2023-07-31 10:11:30 -0500

    Added loading screen to login

--- Context.js
+++ Context.js
@@ -1,5 +1,4 @@
 import React, { createContext, useState } from "react";
-import axios from 'axios'
 
 export const CurrentUserContext = createContext({username: '', password: ''});
 export const DisplayNameContext = createContext('');
@@ -11,39 +10,10 @@ const Providers = props => {
     const [displayName, setDisplayName] = useState('');
     const [userToken, setUserToken] = useState('');
 
-    const getToken = async() => {
-        console.log("Current user: ", currentUser);
-
-        try {
-          const response = await axios.post(`${URL}/wp-json/learnpress/v1/token`, currentUser)
-          if(await validateToken(response.data.token) === 200){
-            setUserToken(response.data.token);
-            console.log(response.data.user_display_name);
-            setDisplayName(response.data.user_display_name);
-          }
-        } catch (error) {
-          console.warn({message: 'validation error', error})
-        }
-      }
-
-    const validateToken = async(token) => {
-        try {
-          const response = await axios.post(`${URL}/wp-json/learnpress/v1/token/validate`,{},{
-            headers:{
-              Authorization: `Bearer ${token}`
-            }
-          })
-          return(response.data.data.status)
-        } catch (error) {
-          return({error})
-        }
-      }
-    
-
     return (
         <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
-          <UserTokenContext.Provider value={{ getToken, validateToken, userToken }}>
-            <DisplayNameContext.Provider value={{ displayName }}>
+          <UserTokenContext.Provider value={{ userToken, setUserToken }}>
+            <DisplayNameContext.Provider value={{ displayName, setDisplayName }}>
               {props.children}
             </DisplayNameContext.Provider>
           </UserTokenContext.Provider>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,77 +1,123 @@
 import { StyleSheet, Text, TextInput, View, Image, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 import Button from '../components/Button';
-
-import { CurrentUserContext } from '../Context';
-import { UserTokenContext } from '../Context';
+import { CurrentUserContext, UserTokenContext, DisplayNameContext, URL } from '../Context';
+import { WindowHeight } from '../Dimensions';
 
 const LoginScreen = ({setIsLoggedIn}) => {
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-  const { getToken, userToken, validateToken } = useContext(UserTokenContext);
+  const { userToken, setUserToken } = useContext(UserTokenContext);
+  const { setDisplayName } = useContext(DisplayNameContext);
+  const [isLoading, setLoading] = useState(false);
 
+  const Login = async() => {
+    setLoading(true);
+
+    try {
+      const response = await axios.post(`${URL}/wp-json/learnpress/v1/token`, currentUser)
+      
+      if (response.status === 200){
+        // Gets token information
+        const token = response.data.token
+        // Gets user's display name to pass with token later
+        setDisplayName(response.data.user_display_name);
+        setUserToken(token);
+      }
+    } catch(error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  
   useEffect(() => {
     if (userToken !== '') {
       const isValid = validateToken(userToken);
+
       if (isValid) {
-        setIsLoggedIn(true)
-        console.log("User token: ", userToken);
+        setIsLoggedIn(true);
       }
     }
-  }, [userToken])
+  }, [userToken]);
 
-  return (
-    <View style={styles.container}>
-      <KeyboardAvoidingView behavior="padding">
-        <View style={styles.container}>
-            <Image
-              style={styles.selenaLogo}
-              source={require('../assets/selena-logo.png')}/>
+  const validateToken = async(token) => {
+    try {
+      const response = await axios.post(`${URL}/wp-json/learnpress/v1/token/validate`,{},{
+        headers:{
+          Authorization: `Bearer ${token}`
+        }
+      })
+      return(response.data.data.status)
 
-            <Text style={styles.title}>MySelena</Text>
+    } catch (error) {
+      return({error})
+    }
+  }
 
-            <View>
-              <View style={styles.inputContainer}>
-                <Image
-                  style={styles.logo}
-                  source={require('../assets/user.png')}
-                />
-                <TextInput
-                  placeholder='Username or email'
-                  value={currentUser.username}
-                  onChangeText={(input) => setCurrentUser({...currentUser, username: input})}
-                  style={styles.input}
-                  placeholderTextColor='#8E8E8E'
-                />
-              </View>
-
-              <View style={styles.inputContainer}>
-                <Image
-                  style={styles.logo}
-                  source={require('../assets/pass.png')}
-                />
-                <TextInput
-                  placeholder='Password'
-                  value={currentUser.password}
-                  onChangeText={(input) => setCurrentUser({...currentUser, password: input})}
-                  style={styles.input}
-                  placeholderTextColor='#8E8E8E'
+  if (isLoading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size='large' color='#aaa'/>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <KeyboardAvoidingView behavior="padding">
+          <View style={styles.container}>
+              <Image
+                style={styles.selenaLogo}
+                source={require('../assets/selena-logo.png')}/>
+  
+              <Text style={styles.title}>MySelena</Text>
+  
+              <View>
+                <View style={styles.inputContainer}>
+                  <Image
+                    style={styles.logo}
+                    source={require('../assets/user.png')}
+                  />
+                  <TextInput
+                    placeholder='Username or email'
+                    value={currentUser.username}
+                    onChangeText={(input) => setCurrentUser({...currentUser, username: input})}
+                    style={styles.input}
+                    placeholderTextColor='#8E8E8E'
+                  />
+                </View>
+  
+                <View style={styles.inputContainer}>
+                  <Image
+                    style={styles.logo}
+                    source={require('../assets/pass.png')}
+                  />
+                  <TextInput
+                    placeholder='Password'
+                    value={currentUser.password}
+                    onChangeText={(input) => setCurrentUser({...currentUser, password: input})}
+                    style={styles.input}
+                    placeholderTextColor='#8E8E8E'
+                  />
+                </View>
+                
+                
+                <Button 
+                title='Login' 
+                onPress={Login} 
+                buttonStyle={{width: 250, height: 50}}
                 />
               </View>
               
-              
-              <Button 
-              title='Login' 
-              onPress={getToken} 
-              buttonStyle={{width: 250, height: 50}}
-              />
-            </View>
-            
-            <Text style={{color: '#8E8E8E'}}>Don't have an Account? <Text style={{color: '#FFC700'}}>Sign Up</Text></Text>
-        </View>
-      </KeyboardAvoidingView>
-    </View>
-  );
+              <Text style={{color: '#8E8E8E'}}>Don't have an Account? <Text style={{color: '#FFC700'}}>Sign Up</Text></Text>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    );
+  }
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -116,6 +162,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#202020',
     fontWeight: 'bold',
+  },
+  loader: {
+    marginVertical: WindowHeight/2,
+    alignItems: 'center'
   },
 });
 
